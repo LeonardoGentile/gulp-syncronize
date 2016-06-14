000780205d4774f918a3113360d7a6fedefd11e1
StevenTheEVILZ <steventheevilz@gmail.com>
2016-06-13 19:02:21 -0500

    v1.0.3
    Fix folders ending with '/' not working. Added comments. Made 'del' synchronous to display more accurate task time. Cleaned up all the code. Fixed jshint warning about ES6.

--- index.js
+++ index.js
@@ -1,42 +1,68 @@
+// jshint esversion: 6
+
 var del = require('del');
 var glob = require("glob");
 
 function main(options) {
+
     var src = options.src;
-    var dest = options.target;
+    if (src.endsWith('/')) {
+        src = src.slice(0, -1);
+    }
     var srcFiles = glob.sync(src + '/**/*');
+
+    var dest = options.target;
+    if (dest.endsWith('/')) {
+        dest = dest.slice(0, -1);
+    }
     var destFiles = glob.sync(dest + '/**/*');
-    var oldFiles = [];
-    var getExt = function(fname) { return fname.substr((~-fname.lastIndexOf('.') >>> 0) + 2); };
-    var afterPeriod = str.substr(str.indexOf('.') + 1);
-    var getNoExt = function (fname) { return fname.slice(0, ~(getExt(fname).length))};
+
+    var exclude = options.exclude;
+    if (exclude === undefined || exclude === null) exclude = [];
+
     var pre = options.extensions;
     if (pre === undefined || pre === null) pre = {};
-    for (var i = 0; i < srcFiles.length; i++) { srcFiles[i] = srcFiles[i].replace(src + '/', ''); } //Get rid of folders
-    for (var i = 0; i < destFiles.length; i++) { destFiles[i] = destFiles[i].replace(dest + '/', ''); } //Get rid of folders
+
+    for (var i = 0; i < srcFiles.length; i++) { srcFiles[i] = srcFiles[i].replace(src + '/', ''); } // Gets rid of src path prefix
+    for (var j = 0; j < destFiles.length; j++) { destFiles[j] = destFiles[j].replace(dest + '/', ''); } // Gets rid of dest path prefix
+
+    function escapeRegExp(str) {
+        return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+    }
     function replaceAll(str, find, replace) {
         return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
     }
 
-    for (var destFile of destFiles) { //Checks that every file in the folder also exists in the src folder
-        if (srcFiles.indexOf(destFile) == -1) { //File does not exist in the src folder; file might be old
-            var destFile2 = destFile + '.';
-            for (var key of Object.keys(pre)) {
-                if (key != '') {
+    var oldFiles = [];
+
+    for (var destFile of destFiles) { // Checks that every file in the folder also exists in the src folder
+        if (srcFiles.indexOf(destFile) == -1) { // File does not exist in the src folder; file might be old
+
+            var destFile2 = destFile + '.'; // Add period suffix to make extension algorithm work
+
+            for (var key of Object.keys(pre)) { // Extension algorithm
+                if (pre[key] !== '') {
                     destFile2 = replaceAll(destFile2, '.' + key + '.', '.' + pre[key] + '.');
                 }
-                if (key === '') {
-                    destFile2 = replaceAll(destFile2, '.' + key + '.', '' + '.');
+                if (pre[key] === '') {
+                    destFile2 = replaceAll(destFile2, '.' + key + '.', '.');
                 }
             }
-            destFile2 = destFile2.slice(0, -1);
+
+            destFile2 = destFile2.slice(0, -1); // Remove period prefix
+
             if (srcFiles.indexOf(destFile2) == -1) {
-                oldFiles.push(destFile); //Adds file to the oldFiles list if file doesn't exist in src and doesn't match any preprocessor extension
+                oldFiles.push(destFile); // Adds file to the oldFiles list if file doesn't exist in src and doesn't match any preprocessor extension
             }
         }
     }
 
-    for (var i = 0; i < oldFiles.length; i++) { oldFiles[i] = dest + '/' + oldFiles[i]; }
-    del(oldFiles);
+    for (var k = 0; k < oldFiles.length; k++) { oldFiles[k] = dest + '/' + oldFiles[k]; } // Add back dest path prefix
+    for (var exFile of exclude) { // Remove excluded files
+        for (var dFile of oldFiles) {
+            if (exFile === dFile.split("/").pop()) oldFiles.splice(oldFiles.indexOf(dFile), 1);
+        }
+    }
+    del.sync(oldFiles); // Delete all the things
 }
 module.exports = main;
